55b7bee2957b71d18dface02c0ea1d71a8a0f9a7
Lab05_etec <Lab05_etec@etec>
2025-03-21 22:38:57 -0300

    exercicios1

--- formulario.php
+++ formulario.php
@@ -68,6 +68,9 @@
 </button>
 <br><br>
 
+
+
+
 <h1>formulario 1</h1>
 </form>
 
@@ -75,6 +78,50 @@
 <label>nome</label>
 <input type="text" name="nome" value=""/>
 <br />
+<label> idade </label>
+<input type="text" name="idade" value="" />
+<br />
+<button type="submit">
+    logar
+</button>
+
+
+
+<h1>formulario 2</h1>
+
+<form action="salvar_dados.php" method="get">
+<label>valor1</label>
+<input type="number" name="n1" id="n1" value=""/>
+<br />
+<label> valor2 </label>
+<input type="number" name="n2" id="n2" value="" />
+<br />
+<button type="submit">
+    logar
+</button>
+
+
+
+<h1>formulario 3</h1>
+</form>
+
+<form action="salvar_dados.php" method="get">
+<label>usuario</label>
+<input type="text" name="usu" value=""/>
+<br />
+<button type="submit">
+    logar
+</button>
+
+
+<h1>formulario 1</h1>
+</form>
+<h3>matheus</h3>
+<form action="salvar_dados.php" method="get">
+<label>nome</label>
+<input type="text" name="nome" value=""/>
+<br />
+<h3>123</h3>
 <label> idade </label>
 <input type="text" name="idade" value="" />
 <br />
--- salvar_dados.php
+++ salvar_dados.php
@@ -1,13 +1,32 @@
 <?php
-
+//test
     echo $_GET["nome"];
     echo "<br/>";
     echo $_GET["idade"];
 
 
+//exercicio 1
     echo $_POST["login"];
     echo "<br/>";
     echo $_POST["senha"];
+
+
+//exercicio 2
+    $v1= $_GET["n1"];
+    echo "<br/>";
+    $v2=$_GET["n2"];
+
+    $soma = $v1 + $v2;
+    echo "a soma $v1 + $v2 = $soma";
+    echo "<br/>";
+
+
+//exercicio 3
+    $oi = $_GET["usu"];
+    echo "ola, $oi";
+    echo "<br/>";
+
+//exercicio 4
 
 
 
